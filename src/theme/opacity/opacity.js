e0bfb430faabbc6f21f450ae17bcc6d46fc986a3
lukharsch <lukasharsch@web.de>
2022-09-08 11:14:29 +0200

    Patched opacity bug

--- src/theme/opacity/opacity.js
+++ src/theme/opacity/opacity.js
@@ -1,28 +1,18 @@
 /* SPDX-License-Identifier: MIT */
 module.exports = {
-  surface: {
-    low: 'var(--opacity-application-surface-low)',
-    medium: 'var(--opacity-application-surface-medium)',
-    high: 'var(--opacity-application-surface-high)',
-  },
-  text: {
-    low: 'var(--opacity-application-text-low)',
-    medium: 'var(--opacity-application-text-medium)',
-    high: 'var(--opacity-application-text-high)',
-    contrast: {
-      low: 'var(--opacity-application-text-contrast-low)',
-      medium: 'var(--opacity-application-text-contrast-medium)',
-      high: 'var(--opacity-application-text-contrast-high)',
-    }
-  },
-  icon: {
-    low: 'var(--opacity-application-icon-low)',
-    medium: 'var(--opacity-application-icon-medium)',
-    high: 'var(--opacity-application-icon-high)',
-    contrast: {
-      low: 'var(--opacity-application-icon-contrast-low)',
-      medium: 'var(--opacity-application-icon-contrast-medium)',
-      high: 'var(--opacity-application-icon-contrast-high)',
-    }
-  }
-}
+  "surface-low": "var(--opacity-application-surface-low)",
+  "surface-medium": "var(--opacity-application-surface-medium)",
+  "surface-high": "var(--opacity-application-surface-high)",
+  "text-low": "var(--opacity-application-text-low)",
+  "text-medium": "var(--opacity-application-text-medium)",
+  "text-high": "var(--opacity-application-text-high)",
+  "text-contrast-low": "var(--opacity-application-text-contrast-low)",
+  "text-contrast-medium": "var(--opacity-application-text-contrast-medium)",
+  "text-contrast-high": "var(--opacity-application-text-contrast-high)",
+  "icon-low": "var(--opacity-application-icon-low)",
+  "icon-medium": "var(--opacity-application-icon-medium)",
+  "icon-high": "var(--opacity-application-icon-high)",
+  "icon-contrast-low": "var(--opacity-application-icon-contrast-low)",
+  "icon-contrast-medium": "var(--opacity-application-icon-contrast-medium)",
+  "icon-contrast-high": "var(--opacity-application-icon-contrast-high)",
+};
